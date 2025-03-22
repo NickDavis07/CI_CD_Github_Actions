d7a120b65621739744d8f569fb0307e6c0a2ddac
Nick Davis <nickwagner@live.com>
2025-03-22 15:39:26 -0600

    minor changes to troubleshoot issues on render

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,8 +1,10 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config(); // Ensure this is called before accessing process.env
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz';
+
+mongoose.connect(MONGODB_URI);
 
 export default mongoose.connection;
